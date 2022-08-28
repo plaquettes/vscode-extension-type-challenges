a4b0482b6129e1128292a90964733057b1d580ac
Rain120 <1085131904@qq.com>
2022-08-28 21:26:06 +0800

    feat: explorer

--- src/explorer/TypeChallengesNode.ts
+++ src/explorer/TypeChallengesNode.ts
@@ -1,67 +1,52 @@
 import * as path from 'path';
-import {Command, ThemeIcon, TreeItem, TreeItemCollapsibleState, Uri} from 'vscode';
-import {QuestionProps} from '../api/question';
+import {
+  Command,
+  ThemeIcon,
+  TreeItem,
+  TreeItemCollapsibleState,
+  Uri
+} from 'vscode';
+import { QuestionProps } from '../api/question';
 
 export class TypeChallengesNode {
-    data: QuestionProps;
+  data: QuestionProps;
 
-    constructor(data: QuestionProps) {
-        this.data = data;
-    }
+  constructor(data: QuestionProps) {
+    this.data = data;
+  }
 
-    public get label(): string {
-        return this.data.label;
-    }
+  public get label(): string {
+    return this.data.label;
+  }
 
-    public get name(): string {
-        return this.data.name;
-    }
+  public get name(): string {
+    return this.data.name;
+  }
 
-    public get previewCommand(): Command {
-        return {
-            title: 'Preview Question',
-            command: 'type-challenges.pickQuestion',
-            arguments: [this],
-        };
-    }
+  public get content(): string {
+    return this.data.content!;
+  }
 
-    public get iconPath(): string | Uri | {light: string | Uri; dark: string | Uri} | ThemeIcon {
-        return path.join(__filename, '../../', 'public', 'assets', 'question.svg');
-    }
+  public get previewCommand(): Command {
+    return {
+      title: 'Preview Question',
+      command: 'type-challenges.pickQuestion',
+      arguments: [this]
+    };
+  }
 
-    public get uri(): Uri {
-        return Uri.from({
-            scheme: 'type-challenges',
-            path: `/${this.label}`, // path must begin with slash /
-        });
-    }
+  public get iconPath():
+    | string
+    | Uri
+    | { light: string | Uri; dark: string | Uri }
+    | ThemeIcon {
+    return path.join(__filename, '../../', 'public', 'assets', 'question.svg');
+  }
+
+  public get uri(): Uri {
+    return Uri.from({
+      scheme: 'type-challenges',
+      path: `/${this.label}` // path must begin with slash /
+    });
+  }
 }
-
-// export class TypeChallengesNode extends TreeItem {
-//     constructor(
-//         public readonly label: string,
-//         public readonly collapsibleState: TreeItemCollapsibleState
-//     ) {
-//         super(label, collapsibleState);
-//     }
-
-//     iconPath = {
-//         light: path.join(__filename, '../', 'public', 'assets', 'question.svg'),
-//         dark: path.join(__filename, '../', 'public', 'assets', 'question.svg'),
-//     };
-
-//     command = {
-//         title: this.label,
-//         command: 'type-challenges.pickQuestion',
-//         tooltip: this.label,
-//         arguments: [this],
-//     };
-
-//     public get uri(): Uri {
-//         return Uri.from({
-//             scheme: 'type-challenges',
-//             authority: 'tree-node',
-//             path: `/${this.label}`, // path must begin with slash /
-//         });
-//     }
-// }
--- src/explorer/TypeChallengesTreeProvider.ts
+++ src/explorer/TypeChallengesTreeProvider.ts
@@ -1,79 +1,91 @@
 import {
-    TreeItem,
-    TreeDataProvider,
-    window,
-    EventEmitter,
-    Event,
-    ExtensionContext,
-    TreeItemCollapsibleState,
+  TreeItem,
+  TreeDataProvider,
+  window,
+  EventEmitter,
+  Event,
+  ExtensionContext,
+  TreeItemCollapsibleState
 } from 'vscode';
 
-import {getQuestions, QuestionProps} from '../api/question';
-import {error} from '../utils/toast';
-import {TypeChallengesNode} from './TypeChallengesNode';
+import { getQuestions, QuestionProps } from '../api/question';
+import { error } from '../utils/toast';
+import { TypeChallengesNode } from './TypeChallengesNode';
 
 const formatName = (name: string = '') => {
-    const current = name.split('-');
-    current[0] = `${Number(current[0])}`;
+  const current = name.split('-');
+  current[0] = `${Number(current[0])}`;
 
-    return current.join(' ');
+  return current.join(' ');
 };
 
-export class TypeChallengesTreeProvider implements TreeDataProvider<TypeChallengesNode> {
-    protected readonly viewType: string = 'typeChallenges.TreeView';
+export class TypeChallengesTreeProvider
+  implements TreeDataProvider<TypeChallengesNode>
+{
+  protected readonly viewType: string = 'typeChallenges.TreeView';
 
-    private context: ExtensionContext;
+  private context: ExtensionContext;
 
-    constructor(context: ExtensionContext) {
-        this.context = context;
-    }
+  constructor(context: ExtensionContext) {
+    this.context = context;
+  }
 
-    private onDidChangeTreeDataEvent: EventEmitter<TypeChallengesNode | undefined | null> =
-        new EventEmitter<TypeChallengesNode | undefined | null>();
+  private onDidChangeTreeDataEvent: EventEmitter<
+    TypeChallengesNode | undefined | null
+  > = new EventEmitter<TypeChallengesNode | undefined | null>();
 
-    public readonly onDidChangeTreeData: Event<TypeChallengesNode | null | undefined> =
-        this.onDidChangeTreeDataEvent.event;
+  public readonly onDidChangeTreeData: Event<
+    TypeChallengesNode | null | undefined
+  > = this.onDidChangeTreeDataEvent.event;
 
-    public initialize(context: ExtensionContext): void {
-        this.context = context;
-    }
+  public initialize(context: ExtensionContext): void {
+    this.context = context;
+  }
 
-    public async refresh(): Promise<void> {
-        this.onDidChangeTreeDataEvent.fire(null);
-        this.getChildren();
-    }
+  public async refresh(): Promise<void> {
+    this.getChildren();
 
-    getTreeItem(element: TypeChallengesNode): TreeItem | Thenable<TreeItem> {
-        return {
-            label: element.label ?? element.name,
-            // collapsibleState: TreeItemCollapsibleState.Collapsed,
-            iconPath: element.iconPath,
-            command: element.previewCommand,
-            resourceUri: element.uri,
-        };
-    }
+    this.onDidChangeTreeDataEvent.fire(null);
+  }
 
-    getChildren(
-        element?: TypeChallengesNode | undefined
-    ): import('vscode').ProviderResult<TypeChallengesNode[]> {
-        return getQuestions()
-            .then(({data}) => {
-                return (data ?? []).map(
-                    (q: QuestionProps) =>
-                        new TypeChallengesNode({
-                            ...q,
-                            label: formatName(q.name),
-                        })
-                );
+  getTreeItem(element: TypeChallengesNode): TreeItem | Thenable<TreeItem> {
+    return {
+      label: element.label ?? element.name,
+      // collapsibleState: TreeItemCollapsibleState.Collapsed,
+      iconPath: element.iconPath,
+      command: element.previewCommand,
+      resourceUri: element.uri
+    };
+  }
+
+  getChildren(
+    element?: TypeChallengesNode | undefined
+  ): import('vscode').ProviderResult<TypeChallengesNode[]> {
+    return getQuestions()
+      .then(({ data }) => {
+        return (data ?? []).map(
+          (q: QuestionProps) =>
+            new TypeChallengesNode({
+              ...q,
+              label: formatName(q.name)
             })
-            .catch(err => {
-                error(err?.message ?? err);
-            });
-    }
+        );
+      })
+      .catch((err) => {
+        error(err?.message ?? err);
+      });
+  }
 
-    public static initTreeViewItem(context: ExtensionContext) {
-        const typeChallengesTreeProvider = new TypeChallengesTreeProvider(context);
+  public static initTreeViewItem(
+    context: ExtensionContext
+  ): TypeChallengesTreeProvider {
+    const typeChallengesTreeProvider = new TypeChallengesTreeProvider(context);
 
-        window.registerTreeDataProvider('type-challenges', typeChallengesTreeProvider);
-    }
+    window.registerTreeDataProvider(
+      'type-challenges',
+      typeChallengesTreeProvider
+    );
+
+    return typeChallengesTreeProvider;
+  }
 }
