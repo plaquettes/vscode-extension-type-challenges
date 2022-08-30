90298112aac8845abef1db3e417567c8c4abe1f5
Rain120 <1085131904@qq.com>
2022-08-30 20:03:00 +0800

    feat: utils about toast, request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,62 +1,65 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import {API_PATH, DEFAULT_REF, GITHUB} from '../const/api';
+import { BASE_URL, DEFAULT_REF } from '../const/api';
+import { warning } from './toast';
 
 const createAxios = (props: AxiosRequestConfig = {}) => {
-    return axios.create({
-        baseURL: `${GITHUB}${API_PATH}`,
-        withCredentials: true,
-        timeout: 5000,
-        ...props,
-        params: {
-            ...props.params,
-            ref: DEFAULT_REF
-        },
-    });
+  return axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,
+    timeout: 5000,
+    ...props,
+    params: {
+      ...props.params,
+      ref: DEFAULT_REF
+    }
+  });
 };
 
 const axiosInstance = createAxios();
-const RETRY_TIME = 3;
+const RETRY_TIME = 10;
 const RETRY_DELAY = 1000;
 
 axiosInstance.interceptors.request.use(
-    config => {
-        config.params.ref = DEFAULT_REF;
+  (config) => {
+    config.params.ref = DEFAULT_REF;
 
-        return config;
-    },
-    err => {
-        return Promise.reject(err);
-    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
 );
 
 axiosInstance.interceptors.response.use(
-    res => {
-        if (res.status !== 200) {
-            // @ts-ignore
-            res.config.requestCount = res.config.requestCount || 0;
+  (res) => {
+    if (res.status !== 200) {
+      // @ts-ignore
+      res.config.requestCount = res.config.requestCount || 0;
 
-            // @ts-ignore
-            if (res.config.requestCount < RETRY_TIME) {
-                // @ts-ignore
-                res.config.requestCount++;
+      // @ts-ignore
+      if (res.config.requestCount < RETRY_TIME) {
+        // @ts-ignore
+        warning(`Retry ${res.config.requestCount} times`);
+        // @ts-ignore
+        res.config.requestCount++;
 
-                let retry = new Promise(resolve => {
-                    setTimeout(() => {
-                        resolve(res);
-                    }, RETRY_DELAY || 100);
-                });
+        let retry = new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(res);
+          }, RETRY_DELAY || 100);
+        });
 
-                return retry.then(() => {
-                    return axios(res.config);
-                });
-            }
-        } else {
-            return Promise.resolve(res);
-        }
-    },
-    err => {
-        return Promise.reject(err);
+        return retry.then(() => {
+          return axios(res.config);
+        });
+      }
+    } else {
+      return Promise.resolve(res);
     }
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
 );
 
 export const get = axiosInstance.get;
